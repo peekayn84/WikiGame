21d0467473b22b9f1c7ad7d6ad7cabe475c461c9
peekayn84 <74303876+peekayn84@users.noreply.github.com>
2022-05-10 09:22:41 +0300

    Add files via upload

---
+++ WikiApi/WikiApi/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WikiApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Pet_door";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(375, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Buddhism_in_Japan";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WikiApi/WikiApi/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WikiApi
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct Page
+        {
+            Page(string title, string address, List<string> path)
+            {
+                Title = title;
+                Address = address;
+                Path = path;
+            }
+
+            public string Title { get; set; }
+            public string Address { get; set; }
+            public List<string> Path { get; set; }
+        }
+        List<Page> pages = new List<Page>();
+        public string getContenidoWeb(string address)
+        {
+
+
+            WebClient client = new WebClient();
+
+            using (Stream stream = client.OpenRead("https://en.wikipedia.org/w/api.php?action=parse&format=xml&page="+ address+ "&prop=text"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+
+            }
+            
+
+        }
+        int findID = -1;
+        public void loadPage(object message) 
+        {
+            string Address = "";
+            int i = (int)message;
+            if (i == -1)
+            {
+                Address = textBox1.Text;
+            }
+            else
+            {
+                Address = pages[i].Address;
+            }
+            try
+            {
+                string pageText = getContenidoWeb(Address); ;
+
+                while (pageText.Contains("href="))
+                {
+                    List<string> curentPath = new List<string>();
+                    if (i == -1)
+                    {
+                        curentPath = new List<string>();
+                    }
+                    else
+                    {
+                        curentPath = pages[i].Path.Distinct().ToList();
+                    }
+
+                    Page page = new Page();
+                    pageText = pageText.Substring(pageText.IndexOf("href") + 12);
+                    page.Address = pageText.Substring(0, pageText.IndexOf('"'));
+                    pageText = pageText.Substring(pageText.IndexOf("title") + 7);
+                    page.Title = pageText.Substring(0, pageText.IndexOf('"'));
+                    curentPath.Add(page.Address);
+                    page.Path = curentPath;
+                    if (!page.Title.Contains("."))
+                        if (!page.Title.Contains("."))
+                            if (!page.Title.Contains("File"))
+                                if (!page.Title.Contains(";"))
+                                    if (!page.Title.Contains("#"))
+                                        if (!page.Title.Contains(":"))
+                                            if (!page.Address.Contains("."))
+                                                if (!page.Address.Contains("."))
+                                                    if (!page.Address.Contains("File"))
+                                                        if (!page.Address.Contains(";"))
+                                                            if (!page.Address.Contains("#"))
+                                                                if (!page.Address.Contains(":"))
+                                                                {
+                                                                    //Console.WriteLine
+                                                                    pages.Add(page);
+                                                                    //Console.WriteLine(page.Path.Count());
+                                                                    //Console.WriteLine(page.Address);
+                                                                    if ((page.Address == textBox2.Text) || (page.Title == textBox2.Text))
+                                                                    {
+
+                                                                        findID = pages.Count - 1;
+                                                                        Console.WriteLine("Found");
+
+
+                                                                        find = true;
+                                                                        break;
+                                                                    }
+                                                                }
+
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        bool find = false;
+        int counter = 1;
+        private int workerThreads;
+        private int portThreads;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadPage(-1);
+            timer1.Enabled = true;
+            //label1.Text += textBox2.Text;
+            /*for (int i = 0; i < pages.Count; i++)
+            {
+                Console.WriteLine(pages[i].Title + " " + pages[i].Address);
+            }*/
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (find)
+            {
+                timer1.Enabled = false;
+                List<string> pathResult = pages[findID].Path;
+                for (int i1 = 0; i1 < pathResult.Count; i1++)
+                {
+                    label1.Text += pathResult[i1] + "=>";
+                }
+                label1.Text = textBox1.Text + "=>" + label1.Text;
+                label1.Text = label1.Text.Substring(0, label1.Text.Length - 2);
+            }
+            Console.WriteLine(counter);
+            for (int i = 0; i < pages.Count; i++)
+            {
+                //Console.WriteLine(pages.Count);
+                if (pages[i].Path.Count == counter)
+                {
+                    loadPage(i);
+                                        //loadPage(i);
+                    if (find)
+                        break;
+                }
+                Thread.Sleep(100);
+            }
+            counter++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
